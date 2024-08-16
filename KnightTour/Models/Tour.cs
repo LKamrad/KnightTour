da2431680fca43cfda18c57b42d607e97267053a
Livia Kamrad <Kwadrat777@gmail.com>
2024-08-16 08:36:28 +0200

    Projektdateien hinzufügen.

---
+++ KnightTour/Models/Tour.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Springertour.Model
+{
+    class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+    internal class Tour
+    {
+
+        List<Position> tour = new List<Position>();
+
+        int[,] field = new int[8, 8];
+
+        int counter = 0;
+
+        public Tour(int x, int y)
+        {
+            AddField(x, y);
+            FirstStepIssue(x,y);
+        }
+        public void StartTour()
+        {
+            while (counter < 64) 
+            {
+                int zahl = 9;
+                foreach (Position p in tour) 
+                {
+                    int temp = PossibleMovesFromTheNextPosition(p);
+                    if (temp < zahl)
+                    {
+                        zahl = temp;
+                    }
+                }
+                foreach (Position p in tour)
+                {
+                    int temp = PossibleMovesFromTheNextPosition(p);
+                    if (temp == zahl)
+                    {
+                        AddField(p.X, p.Y);
+                        break;
+                    }
+                }
+
+                if(tour.Count == 0)
+                {
+                    break;
+                }
+
+            }
+        }
+        public void PrintList()
+        {
+            foreach (Position pos in tour)
+            {
+                Console.WriteLine($"{pos.X} {pos.Y}");
+            } 
+        }
+        public void AddField(int x, int y)
+        {
+           
+            counter++;
+            field[x, y] = counter;
+            tour = new List<Position>();
+            ListOfPossibleMoves(x, y);
+        }
+        public void FirstStepIssue(int x, int y)
+        {
+            if(x == 0 && y == 2)
+            {
+                AddField(x+2, y+1);
+            }
+        }
+
+        public int GetCounter()
+        {
+            return counter;
+        }
+        public int PossibleMovesFromTheNextPosition(Position position)
+        {
+            int x = position.X;
+            int y = position.Y;
+            int possibleMoves = 0;
+            if (x + 2 < 8 && y + 1 < 8)
+            {
+                if (field[x + 2, y + 1] == 0)
+                {
+                    possibleMoves++;
+                }
+
+            }
+            if (x + 2 < 8 && y - 1 >= 0)
+            {
+                if (field[x + 2, y - 1] == 0)
+                {
+                    possibleMoves++;
+                }
+
+            }
+            if (x + 1 < 8 && y + 2 < 8)
+            {
+                if (field[x + 1, y + 2] == 0)
+                {
+                    possibleMoves++;
+                }
+            }
+            if (x + 1 < 8 && y - 2 >= 0)
+            {
+                if (field[x + 1, y - 2] == 0)
+                {
+                    possibleMoves++;
+                }
+            }
+
+            if (x - 2 >= 0 && y + 1 < 8)
+            {
+                if (field[x - 2, y + 1] == 0)
+                {
+                    possibleMoves++;
+                }
+            }
+            if (x - 2 >= 0 && y - 1 >= 0)
+            {
+                if (field[x - 2, y - 1] == 0)
+                {
+                    possibleMoves++;
+                }
+
+            }
+            if (x - 1 >= 0 && y + 2 < 8)
+            {
+                if (field[x - 1, y + 2] == 0)
+                {
+                    possibleMoves++;
+                }
+
+            }
+            if (x - 1 >= 0 && y - 2 >= 0)
+            {
+                if (field[x - 1, y - 2] == 0)
+                {
+                    possibleMoves++;
+                }
+            }
+
+            return possibleMoves;
+        }
+        public void ListOfPossibleMoves(int x, int y) 
+        {
+
+            if (x + 2 < 8 && y + 1 < 8)
+            {
+                if (field[x + 2, y + 1] == 0)
+                {
+                    tour.Add(new Position(x + 2, y + 1));
+                }
+
+            }
+            if (x + 2 < 8 && y - 1 >= 0)
+            {
+                if (field[x + 2, y - 1] == 0)
+                {
+                    tour.Add(new Position(x + 2, y - 1));
+                }
+                
+            }
+            if (x + 1 < 8 && y + 2 < 8)
+            {
+                if (field[x + 1, y + 2] == 0)
+                {
+                    tour.Add(new Position(x + 1, y + 2));
+                }
+            }
+            if (x + 1 < 8 && y - 2 >= 0)
+            {
+                if (field[x + 1, y - 2] == 0)
+                {
+                    tour.Add(new Position(x + 1, y - 2));
+
+                }
+            }
+
+            if (x - 2 >= 0 && y + 1 < 8)
+            {
+                if (field[x - 2, y + 1] == 0)
+                {
+                    tour.Add(new Position(x - 2, y + 1));
+
+                }
+            }
+            if (x - 2 >= 0 && y - 1 >= 0)
+            {
+                if (field[x - 2, y - 1] == 0)
+                {
+                    tour.Add(new Position(x - 2, y - 1));
+                }
+
+            }
+            if (x - 1 >= 0 && y + 2 < 8)
+            {
+                if (field[x - 1, y + 2] == 0)
+                {
+                    tour.Add(new Position(x - 1, y + 2));
+                }
+
+            }
+            if (x - 1 >= 0 && y - 2 >= 0)
+            {
+                if (field[x - 1, y - 2] == 0)
+                {
+                    tour.Add(new Position(x - 1, y - 2));
+                }
+            }
+
+
+        }
+
+        public void Print()
+        {
+            Console.WriteLine();
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+
+                Console.WriteLine($"  |----|----|----|----|----|----|----|----|");
+                Console.Write($"{8 - i} ");
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    Console.Write($"| {field[i, j],2} ");
+                }
+                Console.WriteLine("|");
+            }
+            Console.WriteLine("  |----|----|----|----|----|----|----|----|");
+            Console.WriteLine("  |----|----|----|----|----|----|----|----|");
+            Console.WriteLine("  |  A |  B |  C |  D |  E |  F |  G |  H |");
+
+        }
+
+
+    }
+}
---
+++ KnightTour/Program.cs
@@ -0,0 +1,112 @@
+﻿using Springertour.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Springertour
+{
+    internal class Program
+    {
+        static void AFGTest()
+        {
+            for(int i = 0; i< 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Tour tour = new Tour(i, j);
+                    tour.StartTour();
+                    //tour.Print();
+
+                    Console.Write($"Testing X: {i} Y: {j} ");
+                    if(tour.GetCounter() != 64)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Fehler!!");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Pass!");
+                    }
+                    Console.ResetColor();
+                }
+            }
+            //Tour t2 = new Tour(2, 7);
+            //t2.StartTour();
+            //t2.Print();
+        }
+        
+        static void ManuallInput() 
+        {
+            Console.ResetColor();
+            
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine($"  |----|----|----|----|----|----|----|----|");
+                Console.Write($"{8 - i} ");
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write($"|  0 ");
+                }
+                Console.WriteLine("|");
+            }
+            Console.WriteLine("  |----|----|----|----|----|----|----|----|");
+            Console.WriteLine("  |----|----|----|----|----|----|----|----|");
+            Console.WriteLine("  |  A |  B |  C |  D |  E |  F |  G |  H |");
+            Console.Write("Starposition A-H:");
+
+            char[] array = Console.ReadLine().ToCharArray();
+            int y = array[0] - 65;
+
+
+
+            Console.Write("Starposition 1-8:");
+            int x = int.Parse(Console.ReadLine());
+            x = Math.Abs(x-8);
+
+            Console.Clear();
+
+
+
+
+            Tour tour = new Tour(x, y);
+            tour.StartTour();
+            tour.Print();
+        }
+
+        static void Random()
+        {
+            Random rnd = new Random();
+
+            int i = 0;
+            do {
+                Tour t = new Tour(rnd.Next(0, 8), rnd.Next(0, 8));
+                t.StartTour();
+                t.Print();
+                i++;
+                if(i != 8)
+                {
+                    Console.ForegroundColor += 1;
+                    Console.WriteLine("Press any key to generate the next interation");
+                    Console.WriteLine("\n\n");
+                    Console.ReadKey();
+                }
+            } while (i<8);
+
+
+        }
+        static void Main(string[] args)
+        {
+
+
+            //Manuell();
+            Random();
+            //AFGTest();
+            Console.WriteLine("Press any key to quit");
+            Console.ReadLine();
+
+        }
+    }
+}
